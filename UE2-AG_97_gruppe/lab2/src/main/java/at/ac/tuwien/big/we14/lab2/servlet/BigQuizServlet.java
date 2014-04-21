52bd6715484ef3c4c6b10056a3b162cff74c3e74
benjie <benjamin.kowatsch@gmail.com>
2014-04-21 17:54:39 +0200

    Konfiguration gitignore, web xml und Notes zum aufsetzen und laufen lassen der Applikation auf Web Server

--- UE2-AG_97_gruppe/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
+++ UE2-AG_97_gruppe/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
@@ -1,3 +1,10 @@
 package at.ac.tuwien.big.we14.lab2.servlet;
 
+import javax.servlet.http.HttpServlet;
+
+/*public class BigQuizServlet extends HttpServlet {
+	
+	
+}*/
+
 // Your Servlet implementation
