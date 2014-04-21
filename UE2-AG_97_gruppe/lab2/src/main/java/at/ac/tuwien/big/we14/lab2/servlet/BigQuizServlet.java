33e6188275557039e0d2adfbe2222f5e2c5e8322
benjie <benjamin.kowatsch@gmail.com>
2014-04-21 19:50:30 +0200

    jsp pages

--- UE2-AG_97_gruppe/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
+++ UE2-AG_97_gruppe/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
@@ -1,10 +1,30 @@
 package at.ac.tuwien.big.we14.lab2.servlet;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-/*public class BigQuizServlet extends HttpServlet {
+@WebServlet(name="BigQuiz", urlPatterns = {"/BigQuizServlet"})
+public class BigQuizServlet extends HttpServlet {
 	
 	
-}*/
+	protected void doGet(HttpServletRequest request, HttpServletResponse response){
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response){
+		
+	}
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response){
+		
+	}
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response){
+		
+	}
+	
+}
 
 // Your Servlet implementation
