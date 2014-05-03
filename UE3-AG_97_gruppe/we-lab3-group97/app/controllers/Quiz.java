570119a0518cb27fb083bf1072f2df4b4058de59
benjamin.kowatsch <benjamin.kowatsch@gmail.com>
2014-05-03 23:16:15 +0200

    questions rnd erzeugt, questions werden noch nicht gewechselt

--- UE3-AG_97_gruppe/we-lab3-group97/app/controllers/Quiz.java
+++ UE3-AG_97_gruppe/we-lab3-group97/app/controllers/Quiz.java
@@ -2,10 +2,21 @@ package controllers;
 
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
+import at.ac.tuwien.big.we14.lab2.api.Choice;
+import at.ac.tuwien.big.we14.lab2.api.Question;
+import at.ac.tuwien.big.we14.lab2.api.QuizFactory;
+import at.ac.tuwien.big.we14.lab2.api.QuizGame;
+import at.ac.tuwien.big.we14.lab2.api.User;
+import at.ac.tuwien.big.we14.lab2.api.impl.PlayQuizFactory;
 import models.SimpleUser;
 import play.*;
+import play.api.mvc.Session;
+import play.data.DynamicForm;
 import play.data.Form;
 import play.db.jpa.JPA;
 import play.db.jpa.Transactional;
@@ -14,7 +25,9 @@ import play.mvc.*;
 import views.html.*;
 
 public class Quiz extends Controller {
-
+	
+	private static QuizGame game;
+	
     public static Result authentication() {
         return ok(authentication.render());
     }
@@ -35,16 +48,34 @@ public class Quiz extends Controller {
     public static Result login() {
     	// do login
     	boolean authenticated = true;
+    	
+    	//TODO begin:User dynamisch aus Anmeldeinformation bekommen
+    	User user=new SimpleUser();
+    	user.setName("Hans");
+    	Quiz.session().put("user", user.getName());
+    	//TODO ende
     	if(authenticated) {
     		return ok(index.render());
     	} else {
     		return ok(authentication.render());	// badRequest?
-    	}
-    	
+    	}	
     }
 
-    public static Result question() {
-    	return ok(quiz.render());
+    public static Result quiz() {
+    	User user=new at.ac.tuwien.big.we14.lab2.api.impl.SimpleUser();
+		user.setName(Quiz.session().get("user"));
+		
+    	if(game==null){
+        	QuizFactory factory = new PlayQuizFactory(Play.application().configuration().getString("questions.de"),user);
+    		game=factory.createQuizGame();
+    		game.startNewRound();
+    	}
+    	
+    	Question question = game.getCurrentRound().getCurrentQuestion(user);	   	
+    	List<Choice> choices = question.getAllChoices();
+    	return ok(quiz.render(choices));
     }
+
+    
     
 }
